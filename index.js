3b7f618e63ca0ea349a57601df409c7b2b1855c2
Chris <cmilbert@users.noreply.github.com>
2022-05-15 13:04:38 +0000

    Refactor schema for users, added exercise logging and retrieval.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 
 let mongoose = require('mongoose');
+
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors())
@@ -15,33 +16,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// Data object schemas
-const exerciseSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  description: String,
-  duration: Number,
-  date: Date 
-});
-
+// Schema and model definition
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true }
-})
-
-const logDataSchema = new mongoose.Schema({
-  description: String,
-  duration: Number,
-  date: Date
-})
-
-const logSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  count: Number,
-  log: [logDataSchema]
+  log: [{
+    description:{ type: String, required: true },
+    duration:{ type: Number, required: true },
+    date: { type: Date, required: true }
+  }]
 })
 
-let Exercise = mongoose.model('Exercise', exerciseSchema);
-let User = mongoose.model('User', userSchema);
-let Log = mongoose.model('Log', logSchema);
+const User = mongoose.model("User",userSchema)
 
 // User routes
 app.post("/api/users", (req, res, next) => {
@@ -59,7 +44,7 @@ app.post("/api/users", (req, res, next) => {
 });
 
 app.get("/api/users", (req, res, next) => {
-  const userQuery = User.find({}).select("username id");
+  const userQuery = User.find({}).select("username _id");
   
   userQuery.exec((err, users) => {
     if (err) next(err);
@@ -69,49 +54,49 @@ app.get("/api/users", (req, res, next) => {
 
 app.post("/api/users/:_id/exercises", (req, res, next) => {
   const userId = req.params._id;
+  const date = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
+  const description = req.body.description;
+  const duration = req.body.duration;
+  
+  const exerciseLog = {
+    description,
+    duration,
+    date
+  }
 
-  const userQuery = User.find({_id: userId}).select("username");
-  userQuery.exec((err, users) => {
-    if (err) next(err);
-
-    console.log("user: " + users[0]);
-    console.log("username: " + users[0].username);
-    
-    let date = req.body['date'];
-    console.log("*********");
-    console.log("date: " + date);
-    if(typeof date === 'undefined') {
-      date = new Date();
-      console.log("set date: " + date);
+  User.findByIdAndUpdate(
+    userId,
+    {
+      $push: {
+        log: exerciseLog
+      }
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if(err) return console.log('error updating user:', err);
+      
+      let response = {
+        "_id": userId,
+        "username": updatedUser.username,
+        "date": exerciseLog.date,
+        "duration": parseInt(exerciseLog.duration),
+        "description": exerciseLog.description
+      }
+      
+      res.json(response)
     }
-    console.log("date: " + date);
-    console.log("*********");
-
-    
-    const exerciseDoc = new Exercise({
-      username: users[0].username,
-      description: req.body['description'],
-      duration: req.body['duration'],
-      date: date 
-    });
-
-      exerciseDoc.save(function(err, user) {
-        if (err) next(err);
-        console.log("Response: " + users[0]);
-        res.json(users[0]);
-      });
-  });
+  )
 });
 
-app.post("/api/users/:_id/logs", (req, res, next) => {
+app.get("/api/users/:_id/logs", (req, res, next) => {
   const userId = req.params._id;
 
-  const userQuery = User.find({_id: userId}).select("username");
+  const userQuery = User.find({_id: userId}).select('log');
   userQuery.exec((err, users) => {
-    if (err) next(err);
+    if(err) next(err);
+    res.json(users);
   });
 });
-
 
 
 
